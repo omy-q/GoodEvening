7a4da628d7ec0a00d0c970b438663dce55f9d657
omy-q <olya_aparina@mail.ru>
2021-07-29 19:25:27 +0300

    Создание класса состояний приложения и осуществление получение данных через LiveData

--- app/src/main/java/com/example/goodevening/superview/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/goodevening/superview/viewmodel/MainViewModel.kt
@@ -1,4 +1,33 @@
 package com.example.goodevening.superview.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.goodevening.domainmodel.model.Facade
+import com.example.goodevening.domainmodel.model.FacadeImpl
+import java.lang.Thread.sleep
+import kotlin.random.Random
+
+class MainViewModel(
+    private val liveDataObserver: MutableLiveData<AppState> = MutableLiveData(),
+    val facade: Facade = FacadeImpl()
+) : ViewModel() {
+    fun getLiveData() = liveDataObserver
+
+    fun getFilm() = getDataFromLocalSource()
+
+
+    private fun getDataFromLocalSource() {
+        when ((1..2).random()) {
+            1 -> {
+                liveDataObserver.postValue(AppState.Loading)
+                sleep(3000)
+            }
+            2 -> {liveDataObserver.postValue(AppState.Success(facade.getLocalData())) }
+        }
+//        Thread {
+//            liveDataObserver.postValue(AppState.Loading)
+//            sleep(2000)
+//            liveDataObserver.postValue(AppState.Success(facade.getLocalData()))
+//        }.start()
+    }
 }
