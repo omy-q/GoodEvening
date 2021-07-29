043f4c97791650a42a28964b6b0e78a6578cbce8
omy-q <olya_aparina@mail.ru>
2021-07-29 19:31:54 +0300

    1. Использование ViewBinding при обращении к макету MainActivity.kt 2. Изменение макета activity_main.xml, добавление ToolBar 3. Заготовка для добавление меню в ToolBar 4. Изменение темы приложения для работы с ToolBar

--- app/src/main/java/com/example/goodevening/superview/view/MainActivity.kt
+++ app/src/main/java/com/example/goodevening/superview/view/MainActivity.kt
@@ -3,10 +3,16 @@ package com.example.goodevening.superview.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.goodevening.R
+import com.example.goodevening.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (savedInstanceState == null){
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, MainFragment.newInstance()).commit()
+        }
     }
 }
