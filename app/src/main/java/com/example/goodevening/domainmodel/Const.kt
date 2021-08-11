b65d6dbb6ba02fddee06fca4f01de1ef098e4335
omy-q <olya_aparina@mail.ru>
2021-08-11 20:24:33 +0300

    Получение api-key  для работы с the movie db и временное его хранение в виде константы

---
+++ app/src/main/java/com/example/goodevening/domainmodel/Const.kt
@@ -0,0 +1,5 @@
+package com.example.goodevening.domainmodel
+
+const val THE_MOVIE_DB_API_URL= "https://api.themoviedb.org/3/movie/"
+const val THE_MOVIE_DB_API_KEY_NAME = "api_key"
+const val THE_MOVIE_DB_API_KEY_VALUE = "040a8d24a3e5424c435ad57033f141b3"
