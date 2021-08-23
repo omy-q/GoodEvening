a52e3faa386833d5da52f7d58f27c2b3c77248d3
omy-q <olya_aparina@mail.ru>
2021-08-23 18:50:22 +0300

    Организация передачи данных из фасада во viewmodel

--- app/src/main/java/com/example/goodevening/domainmodel/model/Facade.kt
+++ app/src/main/java/com/example/goodevening/domainmodel/model/Facade.kt
@@ -1,6 +1,7 @@
 package com.example.goodevening.domainmodel.model
 
 import com.example.goodevening.domainmodel.CategoryFilm
+import com.example.goodevening.domainmodel.Film
 import com.example.goodevening.domainmodel.FilmDTO
 
 interface Facade {
@@ -10,4 +11,9 @@ interface Facade {
 
     fun getLocalData() : List<CategoryFilm>
     // временный метод для проверки работы
+
+    fun getDBRecentData() : CategoryFilm
+
+    fun saveRecentFilm(film: Film)
+
 }
