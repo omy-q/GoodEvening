6040399c8d0007eb724ce0f72cfb0ba521e93ba7
omy-q <olya_aparina@mail.ru>
2021-08-31 21:12:09 +0300

    Обработка перехода к новому фрагменту CategoryFragment  через интерфейс OnMoreButtonClickedListener

---
+++ app/src/main/java/com/example/goodevening/superview/view/mainview/OnMoreButtonClickListener.kt
@@ -0,0 +1,7 @@
+package com.example.goodevening.superview.view.mainview
+
+import com.example.goodevening.domainmodel.CategoryFilm
+
+interface OnMoreButtonClickListener {
+    fun onMoreButtonClick(category : CategoryFilm)
+}
