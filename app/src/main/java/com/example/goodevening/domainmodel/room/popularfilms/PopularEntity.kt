e8b505191a3180d321bf754806178801f6afdf75
omy-q <olya_aparina@mail.ru>
2021-09-02 23:32:56 +0300

    Добавление аннотаций для конвертации типа объекта для записи в бд (Почему-то не работает)

--- app/src/main/java/com/example/goodevening/domainmodel/room/popularfilms/PopularEntity.kt
+++ app/src/main/java/com/example/goodevening/domainmodel/room/popularfilms/PopularEntity.kt
@@ -2,11 +2,16 @@ package com.example.goodevening.domainmodel.room.popularfilms
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.goodevening.domainmodel.utils.DateConverter
+import java.util.*
 
 @Entity
 data class PopularEntity (
     @PrimaryKey
     val id: Long,
+//    @TypeConverters(DateConverter::class)
+    val date: Long,
     val filmName: String,
     val filmDescription: String,
     val filmYear: String,
--- app/src/main/java/com/example/goodevening/domainmodel/room/recentfilms/RecentEntity.kt
+++ app/src/main/java/com/example/goodevening/domainmodel/room/recentfilms/RecentEntity.kt
@@ -2,11 +2,16 @@ package com.example.goodevening.domainmodel.room.recentfilms
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
+import com.example.goodevening.domainmodel.utils.DateConverter
+import java.util.*
 
 @Entity
 data class RecentEntity(
     @PrimaryKey
     val id: Long,
+//    @field:TypeConverters(DateConverter::class)
+    val date: Long,
     val filmName: String,
     val filmDescription: String,
     val filmYear: String,
