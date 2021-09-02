9be4f67037dc771d95f8c1ecfbd810d9a34ffce0
omy-q <olya_aparina@mail.ru>
2021-09-02 14:37:26 +0300

    Получение токена в любое время

--- app/src/main/java/com/example/goodevening/superview/view/MainActivity.kt
+++ app/src/main/java/com/example/goodevening/superview/view/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.goodevening.superview.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.goodevening.R
 import com.example.goodevening.databinding.ActivityMainBinding
 import com.example.goodevening.superview.view.mainview.MainFragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.iid.InstanceIdResult
 
 class MainActivity : AppCompatActivity() {
     lateinit var  binding : ActivityMainBinding
@@ -14,5 +18,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         savedInstanceState.let { supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, MainFragment.newInstance()).commit() }
+
+        getClientID()
+    }
+
+    private fun getClientID() {
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener<InstanceIdResult> { task ->
+                if(!task.isSuccessful){
+                    Log.d("token", task.exception.toString())
+                    return@OnCompleteListener
+                } else{
+                    val token : String = task.result!!.token
+                    Log.d("mylogs", "$token")
+                }
+            })
     }
 }
